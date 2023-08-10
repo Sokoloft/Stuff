3cc3aca4a4fbf989d74a166ada8228598c327c42
Ryun Grivois <ryungrivois@gmail.com>
2023-08-09 21:23:33 -0400

    more cleanup

--- hotkeyD_config/scripts/spotify.bat
+++ hotkeyD_config/scripts/spotify.bat
@@ -1,17 +1,11 @@
 :: Name:     spotify.bat
-:: Purpose:  Checks if Toastify is open. If not it will exec Toastify. If it is, it will exec Spotify.
+:: Purpose:  Opens spotify
 :: Author:   https://github.com/Sokoloft/
-:: Revision: October 16/21
+:: Revision: August 9/23
 :: Notes: 	 None.
 ::			  Made possible with https://stackoverflow.com/ copy pasta goodness.
 
-tasklist | find /I "Toastify" 
-if errorlevel 1 (
-        cd "C:\Program Files\Toastify\"
-		start Toastify.exe
-) Else (
-        cd "C:\Users\%username%\AppData\Roaming\Spotify"
-		start Spotify.exe
 
-)
+cd "C:\Users\%username%\AppData\Roaming\Spotify"
+start Spotify.exe
 exit
